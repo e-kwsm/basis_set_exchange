30d404553b930af31f5ad314d542a47126be6ac8
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2020-10-28 04:33:11 +0900

    basis_set_exchange/refconverters/common.py

--- basis_set_exchange/refconverters/common.py
+++ basis_set_exchange/refconverters/common.py
@@ -2,13 +2,14 @@
 Helper functions for writing out basis set references in various formats
 '''
 
+import typing
 from .. import api
 
 _lib_refs = ["pritchard2019a", "feller1996a", "schuchardt2007a"]
 _lib_refs_desc = 'If you downloaded data from the basis set\nexchange or used the basis set exchange python library, please cite:\n'
 
 
-def get_library_citation():
+def get_library_citation() -> typing.Tuple[str, typing.Dict[str, typing.Any]]:
     '''Return a descriptive string and reference data for what users of the library should cite'''
 
     all_ref_data = api.get_reference_data()
